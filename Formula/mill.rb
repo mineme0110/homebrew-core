bea583571d806b9b8f560dca607c639890009e04
Shailesh <53746241+mineme0110@users.noreply.github.com>
2019-11-06 22:33:17 +0000

    ss

--- Formula/mill.rb
+++ Formula/mill.rb
@@ -2,6 +2,7 @@ class Mill < Formula
   desc "Scala build tool"
   homepage "https://www.lihaoyi.com/mill/"
   url "https://github.com/lihaoyi/mill/releases/download/0.5.2/0.5.2-9-ea4f04"
+  sha256 "eac1ac16b0cf3ed996a1ec9948d267b6dee8d1e0ff3ab65ad14da975ed4f5f91"
 
   bottle :unneeded
 
